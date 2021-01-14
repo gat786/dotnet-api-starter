a9cc2796286f3efed17c6cbfc7c9e7fee616f631
ganesh tiwari <gat786@outlook.com>
2021-01-14 17:52:47 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public Message Get()
+        {
+            return new Message
+            {
+                MessageId = 1,
+                MessageText = "Hello World"
+            };
+        }
+    }
+}
---
+++ WebApplication1/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageText { get; set; }
+    }
+}
